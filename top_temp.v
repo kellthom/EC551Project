d713eaadcb5fc0bf9cfdd2b8b463f8afb6506297
kellthom <115281479+kellthom@users.noreply.github.com>
2023-11-30 19:03:57 -0500

    Add files via upload

---
+++ top_temp.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/30/2023 06:08:31 PM
+// Design Name: 
+// Module Name: top_temp
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top_temp(
+    input uart_in,
+    input clk,
+    output [7:0] data_out_r,
+    output [7:0] data_out_g,
+    output [7:0] data_out_b,
+    output [7:0] height,
+    output [7:0] width
+    );
+    
+    wire[7:0] data;
+    wire clk_prime;
+    
+    clk_divider clkdiv(.clk(clk),.clk_out(clk_prime));
+    uart uart1(.clk(clk),.receiver(uart_in),.dout(data));
+    parse parse1(.clk(clk_prime),.reset(reset), .data_in(data), .data_out_r(data_out_r),.data_out_g(data_out_g),.data_out_b(data_out_b),.height(height),.width(width), .data_ready());
+    
+endmodule
