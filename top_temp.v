f52c101951f3504683534cf8c1b927827e0c5329
kellthom <115281479+kellthom@users.noreply.github.com>
2023-12-03 10:59:46 -0500

    Add files via upload

--- top_temp.v
+++ top_temp.v
@@ -30,12 +30,32 @@ module top_temp(
     output [15:0] width
     );
     
+    
+    //Tom
     wire[7:0] data;
     wire clk_prime;
     
+    //Hamed
+    wire[7:0] rom;
+    wire[7:0] out_sobel;
     
-    //clk_divider clkdiv(.clk(clk),.clk_out(clk_prime));
+    //Shining
+    wire hsync;
+    wire vsync;
+    
+    
+    
+    //Tom
+    //Add ready signals for rgb2gray
     uart uart1(.clk(clk),.receive(uart_in),.dout(data),.sample(clk_prime));
     parse parse1(.clk(clk_prime),.reset(reset), .data_in(data), .data_out_r(data_out_r),.data_out_g(data_out_g),.data_out_b(data_out_b),.height(height),.width(width), .data_ready());
+    
+    //Hamed
+    rgb2gray rgb2gray(.clk(clk_prime),.rstn(reset),.start(),.data_in_red(data_out_r),.data_in_green(data_out_g),.data_in_blue(data_out_b),.ready(),.rom(rom));
+    sobel sobel(.ROM_Gray(rom),.clk(clk),.rstn(reset),.start(),.ready(),.out_sobel(out_sobel));
+    
+    //Shining
+    top_module(.clock(clk),.red(out_sobel),.green(out_sobel),.blue(out_sobel),.hsync(hsync),.vsync(vsync));
+    
     
 endmodule
