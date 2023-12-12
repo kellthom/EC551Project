ddb0fa5a87eba34d5765f85c234f2ea2c07cea38
kellthom <115281479+kellthom@users.noreply.github.com>
2023-12-11 19:13:04 -0500

    Add files via upload

---
+++ image and preprecessing/coe_converter.py
@@ -0,0 +1,18 @@
+#Set to desired filenames
+read_file=open("coe_test.txt")
+write_file=open("coe.txt","w")
+
+lines=read_file.readlines()
+
+write_file.write("memory_initialization_radix=16;\n")
+write_file.write("memory_initialization_vector=\n")
+
+for i in range(len(lines)):
+    data=lines[i][0:2]
+    if i==len(lines)-1:
+        write_file.write(data+";\n")
+    else:
+        write_file.write(data+",\n")
+
+read_file.close()
+write_file.close()
