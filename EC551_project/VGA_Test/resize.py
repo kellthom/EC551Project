aa7b44e6e4c0423dfd06b4d1a1c9898527c48b99
kellthom <115281479+kellthom@users.noreply.github.com>
2023-12-12 00:30:12 -0500

    Add files via upload

--- EC551_project/VGA_Test/resize.py
+++ EC551_project/VGA_Test/resize.py
@@ -1,29 +1,31 @@
 def resize(width_old, height_new, width_new):
-    old_file=open("grayscale_image_data.txt")
-    new_file=open("grayscale_image_data_output.txt","w")
+    old_file=open("test.txt")
+    new_file=open("test_output.txt","w")
     height_count=0
     width_count=0
 
     lines=old_file.readlines()
 
-    for i in range(len(lines)):
-
-        #If the new height is achieved, we are done
-        if height_count==height_new:
-            new_file.close()
-            old_file.close()
-            break
+    i=0
+    while(1):        
 
         #If the new width is reached
         if width_count==width_new:
-
+            
             i+=(width_old-width_new)#Skip the rest
 
             width_count=0 #new line
             height_count+=1
 
+            #If the new height is achieved, we are done
+            if height_count==height_new:
+                new_file.close()
+                old_file.close()
+                break
 
+        
         new_file.write(lines[i])
         width_count+=1
+        i+=1
 
-resize(768,480,640)
+resize(5,4,4)
