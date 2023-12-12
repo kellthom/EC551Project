25f9c308e83272843b1463ce7af6a477f8dbded9
ShiningY <148670670+RoseDuke@users.noreply.github.com>
2023-12-12 12:31:02 -0500

    Add files via upload

---
+++ LASTONE/LASTONE.gen/sources_1/bd/BRAM/hdl/BRAM_wrapper.v
@@ -0,0 +1,44 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
+//Date        : Tue Dec 12 11:11:38 2023
+//Host        : ECE-PHO115-126 running 64-bit major release  (build 9200)
+//Command     : generate_target BRAM_wrapper.bd
+//Design      : BRAM_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module BRAM_wrapper
+   (BRAM_PORTA_0_addr,
+    BRAM_PORTA_0_clk,
+    BRAM_PORTA_0_din,
+    BRAM_PORTA_0_we,
+    BRAM_PORTB_0_addr,
+    BRAM_PORTB_0_clk,
+    BRAM_PORTB_0_dout);
+  input [18:0]BRAM_PORTA_0_addr;
+  input BRAM_PORTA_0_clk;
+  input [7:0]BRAM_PORTA_0_din;
+  input [0:0]BRAM_PORTA_0_we;
+  input [18:0]BRAM_PORTB_0_addr;
+  input BRAM_PORTB_0_clk;
+  output [7:0]BRAM_PORTB_0_dout;
+
+  wire [18:0]BRAM_PORTA_0_addr;
+  wire BRAM_PORTA_0_clk;
+  wire [7:0]BRAM_PORTA_0_din;
+  wire [0:0]BRAM_PORTA_0_we;
+  wire [18:0]BRAM_PORTB_0_addr;
+  wire BRAM_PORTB_0_clk;
+  wire [7:0]BRAM_PORTB_0_dout;
+
+  BRAM BRAM_i
+       (.BRAM_PORTA_0_addr(BRAM_PORTA_0_addr),
+        .BRAM_PORTA_0_clk(BRAM_PORTA_0_clk),
+        .BRAM_PORTA_0_din(BRAM_PORTA_0_din),
+        .BRAM_PORTA_0_we(BRAM_PORTA_0_we),
+        .BRAM_PORTB_0_addr(BRAM_PORTB_0_addr),
+        .BRAM_PORTB_0_clk(BRAM_PORTB_0_clk),
+        .BRAM_PORTB_0_dout(BRAM_PORTB_0_dout));
+endmodule
